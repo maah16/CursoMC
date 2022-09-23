730fa218bfab9182c226d6ab99027ed259642e86
Marcela dos Santos <maahsantos.94@gmail.com>
2022-09-23 12:43:56 -0300

    Projeto criado

---
+++ src/test/java/com/cursoMC/CursoMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoMC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
